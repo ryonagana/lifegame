2a2edee28be4d0a41b3b540bd33f2cd85a8b8c25
Joao <joaotande@yahoo.com.br>
2021-12-07 11:19:30 -0300

    Removed unused files

--- main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-extern ALLEGRO_DISPLAY *display;
-extern ALLEGRO_EVENT_QUEUE *event_queue;
-extern ALLEGRO_TIMER *timer;
-
-
-#endif // MAIN_H_INCLUDED
